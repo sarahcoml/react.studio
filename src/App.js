7158eb0ac97365571b9c71a983727a8a68a64878
Sarah Comlan <119999485+sarahcoml@users.noreply.github.com>
2024-03-15 23:04:53 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
+import React, { useState } from 'react';
 import "./App.css";
-import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItemMenu from "./components/BakeryItem";
 
-/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+/* Initialize bakery data images */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
-/* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+
+  const addToCartHandler = (item) => {
+    setCart(currentCart => [...currentCart, item]);
+  };
+
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + item.price, 0);
+  };
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      <h1>Sarah's Bakery</h1>
+      <div className="app-container">
+        <div className="bakery-items-container">
+          {bakeryData.map((item, index) => (
+            <BakeryItemMenu
+              key={index}
+              image={item.image}
+              name={item.title}
+              description={item.description}
+              price={item.price}
+              addToCart={() => addToCartHandler(item)}
+            />
+          ))}
+        </div>
+        <div className="cart-container">
+          <h2>Cart</h2>
+          {cart.length === 0 && <p>The cart is empty.</p>}
+          <ul>
+            {cart.map((item, index) => (
+              <li key={index}>{item.name} - ${item.price}</li>
+            ))}
+          </ul>
+          {cart.length > 0 && <p>Total: ${calculateTotal().toFixed(2)}</p>}
+        </div>
       </div>
     </div>
   );
